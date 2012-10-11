ed6f25295937814abbf16600dcf84b50327a3491
nino zhang <ninozhang@foxmail.com>
2012-10-11 22:27:12 +0800

    touch加入拖动间隔

--- wall.js
+++ wall.js
@@ -2,14 +2,11 @@ var touch = new Touch(),
     events;
 function onDragging(event) {
     var $target = $(event.target),
-        current = event.current,
-        x = current[0].x,
-        y = current[0].y,
-        css = {
-            top: y,
-            left: x
-        };
-    $target.css(css);
+        offset = $target.offset();console.log(offset);
+    $target.css({
+        top: offset.top + event.deltaY,
+        left: offset.left + event.deltaX
+    });
 }
 
 function onHold(event) {
@@ -20,8 +17,20 @@ function onHold(event) {
     $target.addClass('holding');
 }
 
+function onPinching(event) {
+    var $target = $(event.target),
+        height = $target.height(),
+        width = $target.width();
+        direction = event.direction,
+        distance = event.distance,
+        delta = event.delta,
+        scale = event.scale;
+    $target.width(width * (1 + delta)).height(height * (1 + delta));
+}
+
 events = {
     'dragging .image': onDragging,
-    'hold .image': onHold
+    'hold .image': onHold,
+    'pinching .image': onPinching
 };
 touch.on(events);
